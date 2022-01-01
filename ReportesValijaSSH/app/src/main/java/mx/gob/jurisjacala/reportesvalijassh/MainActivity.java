378d494e4d98934aa023a645bec22028f7bc9566
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-01-01 13:37:02 -0600

    -

--- ReportesValijaSSH/app/src/main/java/mx/gob/jurisjacala/reportesvalijassh/MainActivity.java
+++ ReportesValijaSSH/app/src/main/java/mx/gob/jurisjacala/reportesvalijassh/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
 
         setSupportActionBar(binding.toolbar);
 
